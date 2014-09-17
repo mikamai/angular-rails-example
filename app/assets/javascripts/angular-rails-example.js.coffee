fb875352f96f91dc8100404c2a53750408fa9edc
Nicola Racco <nicola@nicolaracco.com>
2014-09-17 14:55:47 +0200

    angular app

---
+++ app/assets/javascripts/angular-rails-example.js.coffee
@@ -0,0 +1,1 @@
+app = angular.module 'angular-rails-example', []
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,7 @@
 //= require jquery
 //= require jquery_ujs
+//= require angular
+//= require angular-route
+//= require angular-resource
+//= require angular-rails-templates
 //= require_tree .
